4719b706b7deb190dd08a6ff426ebfd9314d6f48
LyszczarzKrzysztof <krzysiek.lyszczarz@o2.pl>
2020-07-16 23:30:37 +0200

    Reading & writing websites to html files in multithread mode. Number of threads is 30.

--- src/main/java/OtoDom.java
+++ src/main/java/OtoDom.java
@@ -3,10 +3,19 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 import java.io.*;
 import java.net.URL;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class OtoDom {
 
+    //bez watkow 126871 ms
+    // z wątkami   5506 ms
+
     public static void main(String[] args) throws Exception {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(30);
+
+        long start = System.currentTimeMillis();
 
         URL otodom = new URL("https://www.otodom.pl/sprzedaz/mieszkanie/podkarpackie/");
         BufferedReader in = new BufferedReader(
@@ -35,8 +44,29 @@ public class OtoDom {
             String[] split = content.substring(i).split(".html");
             String link = split[0];
             links.add(link);
-            readWebsite(link,i+".html");
         }
+
+        int i=0;
+
+        for (String link: links) {
+            int finalI = i;
+            executorService.submit(()->{
+                        try {
+                            readWebsite(link, finalI +".html");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+            );
+            i++;
+        }
+
+        executorService.shutdown();
+
+        long end = System.currentTimeMillis();
+
+        System.out.println(end-start);
+        System.out.println(((end-start)/1000)/60);
     }
 
     public static void readWebsite(String link, String fileName) throws IOException {
